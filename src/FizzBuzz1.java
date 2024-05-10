e80cdd52c1d2dff32dc601ee2d0455dc4b53ad06
nalttyan <g22922kn@gm.tsuda.ac.jp>
2024-05-10 16:17:50 +0900

    commit

--- src/FizzBuzz1.java
+++ src/FizzBuzz1.java
@@ -2,7 +2,7 @@ public class FizzBuzz1 {
 	public static void main(String[] args) {
 		for(int i=1;i<=100;i++) {
 			if(i%15==0) {
-				System.out.println("FizzBuzz");
+				System.out.println("FizzBuzz1");
 			} else if (i%3==0){
 				System.out.println("Fizz");
 				
